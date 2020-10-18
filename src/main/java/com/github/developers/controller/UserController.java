9206c7259b90f6e748df702f7c14d252afc33cda
osmar-perez <osmar.perez@dartdigital.com.br>
2020-10-18 20:11:32 -0300

    Feat: Creating UserController Test

--- src/main/java/com/github/developers/controller/UserController.java
+++ src/main/java/com/github/developers/controller/UserController.java
@@ -15,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -38,59 +37,61 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 @RequestMapping("user")
 public class UserController {
 
-  @Autowired
-  private UserService<UserDTO> userService;
+  @Autowired private UserService<UserDTO> userService;
 
-  @Autowired
-  private AuthenticationManager authenticationManager;
+  @Autowired private AuthenticationManager authenticationManager;
 
-  @Autowired
-  private JwtManager jwtManager;
+  @Autowired private JwtManager jwtManager;
 
   @PostMapping("/create")
-  ResponseEntity<UserDTO> createUser(@Valid @RequestBody UserDTO userDTO){
+  ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
     User user = userService.create(userDTO);
-    return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest().path("/{uuid}")
-        .buildAndExpand(user.getUuid()).toUri()).build();
+    return ResponseEntity.created(
+            ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{uuid}")
+                .buildAndExpand(user.getUuid())
+                .toUri())
+        .build();
   }
 
   @PostMapping("/login")
-  ResponseEntity<UserLoginResponseDTO> login(@RequestBody LoginDTO loginDTO){
-    UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(loginDTO.getEmail(), loginDTO.getPassword());
+  ResponseEntity<UserLoginResponseDTO> login(@RequestBody LoginDTO loginDTO) {
+    UsernamePasswordAuthenticationToken token =
+        new UsernamePasswordAuthenticationToken(loginDTO.getEmail(), loginDTO.getPassword());
     Authentication authenticate = authenticationManager.authenticate(token);
     SecurityContextHolder.getContext().setAuthentication(authenticate);
 
-    org.springframework.security.core.userdetails.User userDetails = (org.springframework.security.core.userdetails.User) authenticate.getPrincipal();
-    List<String> roles= userDetails.getAuthorities().stream()
-        .map(authority
-            -> authority.getAuthority())
-        .collect(Collectors.toList());
+    org.springframework.security.core.userdetails.User userDetails =
+        (org.springframework.security.core.userdetails.User) authenticate.getPrincipal();
+    List<String> roles =
+        userDetails.getAuthorities().stream()
+            .map(authority -> authority.getAuthority())
+            .collect(Collectors.toList());
 
     return ResponseEntity.ok(jwtManager.createToken(userDetails.getUsername(), roles));
   }
 
   @GetMapping("/{id}")
-  ResponseEntity<UserDTO> findById(@PathVariable(name = "id") UUID uuid){
+  ResponseEntity<UserDTO> findById(@PathVariable(name = "id") UUID uuid) {
     return ResponseEntity.ok(userService.findById(uuid));
   }
 
   @GetMapping("find-all")
   ResponseEntity<Page<UserDTO>> findAll(
       @RequestParam(value = "page", defaultValue = "0") int page,
-      @RequestParam(value = "size", defaultValue = "15") int size){
+      @RequestParam(value = "size", defaultValue = "15") int size) {
     return ResponseEntity.ok(userService.findAll(PageRequest.of(page, size)));
   }
 
   @DeleteMapping()
-  ResponseEntity<Void> deleteUser(@RequestBody UserDTO userDTO){
+  ResponseEntity<Void> deleteUser(@RequestBody UserDTO userDTO) {
     userService.delete(userDTO);
     return ResponseEntity.noContent().build();
   }
 
   @PutMapping()
-  ResponseEntity<Void> updateUser(@RequestBody UserDTO userDTO){
+  ResponseEntity<Void> updateUser(@RequestBody UserDTO userDTO) {
     userService.update(userDTO);
     return ResponseEntity.noContent().build();
   }
-
 }
